8a83c49037b7eee65750fa89b4d983602c68df5c
RubensZaes <rubenszaes@gmail.com>
2023-10-09 11:13:07 -0300

    :wrench: chore Configuração Informações API

--- src/main/java/br/com/rubenszaes/todolist/config/SpringDocConfiguration.java
+++ src/main/java/br/com/rubenszaes/todolist/config/SpringDocConfiguration.java
@@ -2,6 +2,9 @@ package br.com.rubenszaes.todolist.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,12 @@ public class SpringDocConfiguration {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
-    }
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("ToDo List API")
+                        .description("API Rest da Aplicação ToDoList")
+                        .contact(new Contact()
+                                .name("Rubens Zaes")
+                                .email("rubenszaes@gmail.com")));
+    } //Add @SecurityRequirement(name = "bearer-key") nos controllers que precisão de autenticação
 }
