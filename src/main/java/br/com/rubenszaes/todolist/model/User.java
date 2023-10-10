e7e8c8b04e792dfdd6ee432041d8958123f1d5e7
RubensZaes <rubenszaes@gmail.com>
2023-10-10 11:27:16 -0300

    :sparkles: feat Entity User

--- src/main/java/br/com/rubenszaes/todolist/model/User.java
+++ src/main/java/br/com/rubenszaes/todolist/model/User.java
@@ -1,2 +1,21 @@
-package br.com.rubenszaes.todolist.model;public class User {
+package br.com.rubenszaes.todolist.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
+public class User {
+    @Id @GeneratedValue(generator = "UUID")
+    private UUID id;
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime createAt;
 }
