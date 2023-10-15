642d2fb3961e27d12ba1f55e114f39b362601c91
RubensZaes <rubenszaes@gmail.com>
2023-10-15 09:36:57 -0300

    :sparkles: feat TaskModel

--- src/main/java/br/com/rubenszaes/todolist/model/TaskModel.java
+++ src/main/java/br/com/rubenszaes/todolist/model/TaskModel.java
@@ -1,2 +1,28 @@
-package br.com.rubenszaes.todolist.model;public class TaskModel {
+package br.com.rubenszaes.todolist.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_task")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endtAt;
+    private String priority;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    private String idUser;
 }
