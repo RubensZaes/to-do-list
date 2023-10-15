b02d7b54cea5b7b48e35595ecedfdc921ad0b0e4
RubensZaes <rubenszaes@gmail.com>
2023-10-15 09:37:47 -0300

    :sparkles: feat TaskRepository

--- src/main/java/br/com/rubenszaes/todolist/respository/TaskRepository.java
+++ src/main/java/br/com/rubenszaes/todolist/respository/TaskRepository.java
@@ -1,2 +1,9 @@
-package br.com.rubenszaes.todolist.respository;public class TaskRepository {
+package br.com.rubenszaes.todolist.respository;
+
+import br.com.rubenszaes.todolist.model.TaskModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
 }
